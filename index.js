e7df02f3a0e557179ff20e3c1b274bc9fcbaccea
MuzanJacksonnn <zulianitristan@gmail.com>
2024-11-28 15:03:32 -1000

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get('/api/healthcheck', (req, res) => {
+    res.status(200).send('OK');
+});
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
