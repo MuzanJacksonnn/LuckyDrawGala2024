d03a874cb0ff7027153e18e288a7cfcbdad958fd
MuzanJacksonnn <zulianitristan@gmail.com>
2025-06-26 15:08:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -124,16 +124,6 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://luckydrawgalafkcci.netlify.app');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    return res.status(200).json({});
-  }
-  next();
-});
-
 async function initializeGoogleSheets() {
   try {
     await doc.useServiceAccountAuth({
